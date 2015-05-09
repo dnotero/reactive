d251c52aaab2af9ac6fa5fe1f96f145425433693
dnotero <otero.diego.87@gmail.com>
2015-05-09 04:13:27 -0300

    implement continueWith and continue

--- nodescala/src/main/scala/nodescala/package.scala
+++ nodescala/src/main/scala/nodescala/package.scala
@@ -98,7 +98,14 @@ package object nodescala {
      *  The function `cont` is called only after the current future completes.
      *  The resulting future contains a value returned by `cont`.
      */
-    def continueWith[S](cont: Future[T] => S): Future[S] = ???
+    def continueWith[S](cont: Future[T] => S): Future[S] = {
+      val p = Promise[S]()
+      this.onComplete {
+        p.complete(cont(this))
+      }
+      
+      p.future
+    }
 
     /** Continues the computation of this future by taking the result
      *  of the current future and mapping it into another future.
@@ -106,7 +113,14 @@ package object nodescala {
      *  The function `cont` is called only after the current future completes.
      *  The resulting future contains a value returned by `cont`.
      */
-    def continue[S](cont: Try[T] => S): Future[S] = ???
+    def continue[S](cont: Try[T] => S): Future[S] = {
+      val p = Promise[S]()
+      this.onComplete {
+        p.complete(cont(this))
+      }
+      
+      p.future
+    }
 
   }
 
